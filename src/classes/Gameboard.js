e894b4c42d68109ba614935771f0dfb590af90f4
foufouboy <cissaclement@gmail.com>
2023-04-14 12:42:09 +0200

    Finished Gameboard class first writing

--- src/classes/Gameboard.js
+++ src/classes/Gameboard.js
@@ -4,6 +4,7 @@ const SIZE = 10;
 export default class Gameboard {
     constructor() {
         this.board = [];
+        this.coordsShoted = [];
         this.init();
     } 
 
@@ -20,6 +21,18 @@ export default class Gameboard {
         if (!this.isPlacementPossible(ship, row, col, isVertical)) {
             return false;
         }
+
+        if (isVertical) {
+            for (let i = 0; i < ship.length; i++) {
+                this.board[row + i][col] = ship;
+            }
+        } else {
+            for (let i = 0; i < ship.length; i++) {
+                this.board[row][col + i] = ship;
+            }
+        }
+
+        return true;
     }
 
     isPlacementPossible(ship, row, col, isVertical) {
@@ -35,7 +48,75 @@ export default class Gameboard {
         }
 
         // ships collision
+        if (isVertical) {
+            for (let i = -1; i <= ship.length; i++) {
+                if (this.board[row + i]) {
+                    if (this.board[row + i][col] ||
+                        this.board[row + i][col - 1] ||
+                        this.board[row + i][col + 1]) {
+                        return false; 
+                    }
+                }
+            }
+        } else {
+            for (let i = -1; i <= ship.length; i++) {
+                if (this.board[row][col + i] ||
+                   (this.board[row - 1] && this.board[row - 1][col + i]) ||
+                   (this.board[row + 1] && this.board[row + 1][col + i])) {
+                    return false; 
+                }
+            }
+        }
 
         return true;
+    }
+
+    receiveAttack(row, col) {
+
+        if (row < 0 || SIZE <= row || col < 0 || SIZE <= col) {
+            return false;
+        }
+        
+        if (this.coordsShoted
+            .some(coords => coords.row === row && coords.col === col)) {
+            return false;
+        }
+
+        if (this.board[row][col]) {
+            this.board[row][col].hit();
+        }
+
+        this.coordsShoted.push({row, col});
+        
+        return true;
+    }
+
+    isGameOver() {
+        for (let row = 0; row < SIZE; row++) {
+            for (let col = 0; col < SIZE; col++) {
+                if (this.board[row][col] &&
+                    !this.board[row][col].isSunk()) {
+                    return false;
+                } 
+            }
+        }    
+        return true;
+    }
+
+    isEmpty() {
+        for (let row = 0; row < SIZE; row++) {
+            for (let col = 0; col < SIZE; col++) {
+                if (this.board[row][col]) return false;
+            }
+        }
+
+        return true;
+    }
+
+    printBoard() {
+        // this.board
+        //     .map(row => {console.log(row
+        //     .map(e => e ? "S" : "~"))});
+        console.log(this.board);
     }
 }
--- src/tests/Gameboard.test.js
+++ src/tests/Gameboard.test.js
@@ -1,34 +1,135 @@
 import Gameboard from "../classes/Gameboard.js";
 import Ship from "../classes/Ship.js"
 
-describe("Gameboard", () => {
+let gameboard;
+let ship,
+    ship2,
+    ship3;
 
-    let gameboard;
+beforeEach(() => {
+    gameboard = new Gameboard();  
+    ship = new Ship(5);
+    ship2 = new Ship(3);
+    ship3 = new Ship(2);
+});
 
-    beforeEach(() => {
-        gameboard = new Gameboard();  
-    })
+describe("Gameboard inits correctly", () => {
 
-    test("Gameboard inits correctly", () => {
 
+    test("Gameboard instance is defined", () => {
         expect(gameboard).toBeDefined();
-        expect(gameboard.board).toBeDefined();
-        expect(gameboard.board instanceof Array).toBeTruthy();
-
     });
 
+    test("Gameboard board is defined, and is an array", () => {
+        expect(gameboard.board).toBeDefined();
+        expect(gameboard.board instanceof Array).toBeTruthy();
+    }); 
 
-    test("Gameboard places ship correctly", () => {
-        const ship = new Ship(5);
-        const ship2 = new Ship(3);
 
-        gameboard.place(ship, 0, 0, true);
-        gameboard.place(ship2, 5, 5, false);
+});
 
+describe("Gameboard places ship correctly", () => {
+
+    test("Gameboard.place exists", () => {
+        expect(gameboard.place).toBeDefined();
+    });
+
+    test("Ship can be placed on board", () => {
+        gameboard.place(ship, 0, 0, false);
+
+        for (let i = 0; i < ship.length; i++) {
+            expect(gameboard.board[0][i] instanceof Ship)
+                .toBe(true);
+        }
+    });
+
+    test("Ship can't be placed off board", () => {
         expect(gameboard.place(ship, -1, 0, true)).toBe(false)
         expect(gameboard.place(ship, 0, 10, true)).toBe(false)
 
         expect(gameboard.place(ship2, 5, 9, false)).toBe(false)
         expect(gameboard.place(ship, 8, 8, true)).toBe(false)
     });
+
+    test(
+    "Ship can't collide with other ships, neither be too close", 
+    () => {
+        gameboard.place(ship, 0, 0, true);
+
+        expect(gameboard.place(ship2, 0, 0, false)).toBe(false);
+        expect(gameboard.place(ship2, 0, 1, true)).toBe(false);
+        expect(gameboard.place(ship2, 5, 0, true)).toBe(false);
+        expect(gameboard.place(ship2, 6, 0, true)).toBe(true);
+    });
 });
+
+describe("Gameboard receives attacks properly", () => {
+      
+    test("Gameboard.receiveAttack exists", () => {
+        expect(gameboard.receiveAttack).toBeDefined();
+    });
+
+    test("Cannot shot off board", () => {
+        expect(gameboard.receiveAttack(-1, -1)).toBe(false);
+        expect(gameboard.receiveAttack(2, 2)).toBe(true);
+    })
+
+    test("Shots coords are saved when a shot is made", () => {
+        gameboard.receiveAttack(0, 0);  
+
+        expect(gameboard.coordsShoted).not.toEqual(new Gameboard().coordsShoted);
+    });
+
+    test("Cannot shot on place already shoted", () => {
+        gameboard.receiveAttack(5, 5); 
+        gameboard.receiveAttack(3, 4); 
+        gameboard.receiveAttack(2, 1); 
+
+        expect(gameboard.receiveAttack(1, 1)).toBe(true);
+        expect(gameboard.receiveAttack(2, 1)).toBe(false);
+    });
+
+    test("A shot on a ship increases its hits count", () => {
+        gameboard.place(ship, 2, 2, true);  
+        gameboard.place(ship2, 0, 2, false);  
+
+        gameboard.receiveAttack(5, 2);
+        gameboard.receiveAttack(3, 2);
+        gameboard.receiveAttack(5, 2);
+
+        expect(ship.hits).toEqual(2);
+        expect(ship2.hits).toEqual(0);
+    });
+});
+
+describe("Game over", () => {
+    
+    test("Gameboard.isGameOver exists", () => {
+        expect(gameboard.isGameOver).toBeDefined();  
+    });
+
+    test("Game should be over when all ships are sunk", () => {
+        gameboard.place(ship, 0, 0, false);
+        gameboard.place(ship2, 5, 5, true);
+
+        expect(gameboard.isGameOver()).toBe(false);
+
+        for (let i = 0; i < ship.length; i++) {
+            gameboard.receiveAttack(0, i);
+        }
+
+        for (let i = 0; i < ship2.length; i++) {
+            gameboard.receiveAttack(5 + i, 5);
+        }
+        
+        expect(ship.isSunk()).toBe(true);
+        expect(ship2.isSunk()).toBe(true);
+        expect(gameboard.isGameOver()).toBe(true);
+
+    });
+});
+
+
+
+
+
